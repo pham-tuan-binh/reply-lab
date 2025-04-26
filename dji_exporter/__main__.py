0b9f394ae5e54226e48066533a0041eed414fbc5
pham_tuan_binh <phamtuanbinh1504@gmail.com>
2025-04-26 17:57:42 +0200

    Third

--- dji_exporter/__main__.py
+++ dji_exporter/__main__.py
@@ -16,12 +16,6 @@ waypoints = [
         'lng': 11.499985362, 
         'height': 20,
         'actions': [
-            create_rotate_yaw_action(0),
-            create_gimbal_rotate_action(pitch_angle=0),
-            create_zoom_action(24),
-            create_gimbal_rotate_action(pitch_angle=-58.9),
-            create_rotate_yaw_action(-105.6),
-            create_take_photo_action()
         ]
     },
     {
@@ -29,9 +23,13 @@ waypoints = [
         'lng': 11.500985362, 
         'height': 30,
         'actions': [
-            create_rotate_yaw_action(90),
-            create_gimbal_rotate_action(pitch_angle=-45),
-            create_take_photo_action()
+        ]
+    },
+    {
+        'lat': 48.19114118, 
+        'lng': 11.500985362, 
+        'height': 30,
+        'actions': [
         ]
     }
 ]
